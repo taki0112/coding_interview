10929491506ce5bb8c6a129622e4154689d6066b
Junho Kim <takis0112@gmail.com>
2018-11-24 11:18:23 +0900

    add code

---
+++ src/스택_큐/프린터.py
@@ -0,0 +1,22 @@
+def solution(priorities, location):
+    answer = 0
+    search, c = sorted(priorities, reverse=True), 0
+    while True:
+        for i, priority in enumerate(priorities):
+            s = search[c] # 9
+            if priority == s:
+                c += 1
+                answer += 1
+                if i == location:
+                    break
+        else:
+            continue
+        break
+
+    return answer
+
+priorities = [1, 1, 9, 1, 1, 1]
+location = 0
+
+x = solution(priorities, location)
+print(x)
