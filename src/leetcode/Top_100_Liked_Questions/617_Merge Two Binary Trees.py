7607f629e54b55a574791ffa6d07914e388084fa
Junho Kim <takis0112@gmail.com>
2020-04-13 21:57:07 +0900

    add leetcode

---
+++ src/leetcode/Top_100_Liked_Questions/617_Merge Two Binary Trees.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/merge-two-binary-trees/
+# 617. Merge Two Binary Trees
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
+        if t1 is None:
+            return t2
+        if t2 is None:
+            return t1
+
+        t1.val += t2.val
+        t1.left = self.mergeTrees(t1.left, t2.left)
+        t1.right = self.mergeTrees(t1.right, t2.right)
+
+        return t1
